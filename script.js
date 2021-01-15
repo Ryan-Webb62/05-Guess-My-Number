151a6596b57a1a0eb663c9d5b5babd000ee5ad10
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-01-15 18:48:56 -0500

    Update background upon player win

--- script.js
+++ script.js
@@ -19,10 +19,18 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number! 🚫';
+
+    // When the player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When the player guesses too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -32,6 +40,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You Lost The Game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When the play guesses too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
